9c350f762584aa11e731f37f92cdd741bf387a6c
Bharat <apple@Bharats-MacBook-Pro.local>
2017-10-03 14:24:57 +0530

    Added the form

--- src/App.js
+++ src/App.js
@@ -2,20 +2,44 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div class = "App">
+      <header class = "App-header">
+        <img src = {logo} class="App-logo"/>
+        <h1 class = "App-title">Hello</h1>
+        <p>Just testing</p>
+      </header>
+      <div class="App-intro">{this.props.message}</div>
+    </div>
+    );
+  }
+}
+
+class Login extends Component{
+  render(){
+    return (
+      <div class = "App">
+        <form onsubmit = "">
+          <div class = "">
+            <input type = "text" placeholder = "Username" />
+          </div>
+          <div class= " ">
+            <input type = "password" placeholder = "Password" />
+          </div>
+          <div class = "">
+            <button>Submit</button>
+          </div>
+        </form>
       </div>
     );
   }
 }
 
-export default App;
+export {
+  App,
+  Login
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {App, Login} from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<App message="Bye world!" />, document.getElementById('root'));
+ReactDOM.render(<Login />,document.getElementById('login'));
+
 registerServiceWorker();
